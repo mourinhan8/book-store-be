dd1d8c4ee6e3b5360f3b7bb5827c690083022ee7
Nhan Nguyen Ngoc <sbtc.walkingdead@gmail.com>
2024-04-08 16:01:56 +0700

    vercel

--- server.js
+++ server.js
@@ -1,14 +1,59 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const app = express();
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const swaggerTail = require("./swagger.json");
+const swaggerConfigInit = {
+  swagger: "2.0",
+  info: {
+    version: "1.0.0",
+    title: "Books API document",
+    description: "bookstores backend"
+  },
+  host: `${process.env.BACKEND_HOST}`,
+  basePath: "/api/v1",
+  securityDefinitions: {
+    bearerAuth: {
+      type: "apiKey",
+      name: "x-auth-token",
+      scheme: "",
+      in: "header"
+    }
+  },
+  tags: [
+    {
+      name: "users",
+      description: "API for users"
+    },
+    {
+      name: "books",
+      description: "API for book"
+    },
+    {
+      name: "purchases",
+      description: "API for purchase"
+    }
+  ],
+  schemes: [
+    "http",
+    "https"
+  ],
+  consumes: [
+    "application/json",
+    "multipart/form-data"
+  ],
+  produces: ["application/json"]
+};
 var options = {
   swaggerOptions: {
-    authAction :{ JWT: {name: "JWT", schema: {type: "apiKey", in: "header", name: "Authorization", description: ""}, value: "Bearer <JWT>"} }
+    authAction: { JWT: { name: "JWT", schema: { type: "apiKey", in: "header", name: "Authorization", description: "" }, value: "Bearer <JWT>" } }
   }
+};
+const swaggerDocument = {
+  ...swaggerConfigInit,
+  ...swaggerTail
 };
 // Connection check with db
 connectDB();
